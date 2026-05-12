4618fda2fb6850dbf18ea52e6c66dbc4731c82c6
thomassoftech <thomasgreat0@gmail.com>
2026-05-12 17:22:42 +0100

    change it to the right directory

--- server.js
+++ server.js
@@ -3,9 +3,30 @@ const mongoose = require('mongoose');
 const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
-const port = process.env.PORT || 3000;
+// const { auth, requiresAuth } =require('express-openid-connect');
+const port = process.env.PORT || 3000; 
 
-app.use('/', require('./routes'));
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: process.env.SECRET,
+//   baseURL: process.env.BASE_URL,
+//   clientID: process.env.CLIENT_ID,
+//   issuerBaseURL: process.envISSUER_BASE_URL,
+// };
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+// app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router.
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// })
+// app.get('/profile', requiresAuth(), (req, res) => {
+//   res.send(JSON.stringify(req.oidc.user));
+// });
+
+ app.use('/', require('./routes'));
 
 const startServer = async () => {
   try {
